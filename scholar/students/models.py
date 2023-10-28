e5e35b6031cd35eaa08fce6574de98167c3281dd
JohnDarwinMeloMelo <jhon.darwin.melo105@gmail.com>
2023-10-27 20:01:44 -0500

    models

--- scholar/students/models.py
+++ scholar/students/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 class User(models.Model):
-    emaIl=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
     password=models.CharField(max_length=100)
-    
+    dn1=models.CharField(max_length=10,default='')
 
