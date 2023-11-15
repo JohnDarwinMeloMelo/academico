1b110f95f27ef73887312305bd110630d9300399
JohndarwinMeloMelo <jhon.darwin.melo105@gmail.com>
2023-11-14 19:56:30 -0500

    models

--- scholar/students/models.py
+++ scholar/students/models.py
@@ -4,7 +4,7 @@ import datetime
 
 # Create your models here.
 class Users(models.Model):
-    email = models.CharField(max_length=100)
+    email = models.CharField(max_length=200)
     password = models.CharField(max_length=500)
     status = models.BooleanField(null=True)
     created_at = models.DateTimeField(default=datetime.datetime.now, null=False)
@@ -27,8 +27,19 @@ class identification_types(models.Model):
     updated_at = models.DateTimeField(default=datetime.datetime.now, null=False)
     deleted_at = models.DateTimeField(null=True)
 
+class cities(models.Model):
+    name = models.CharField(max_length=110, null=False)
+    abrev = models.CharField(max_length=10, null=False)
+    descrip = models.CharField(max_length=10, null=False)
+    id_dept = models.IntegerField(null=False)
+    created_at = models.DateTimeField(default=datetime.datetime.now, null=False)
+    updated_at = models.DateTimeField(default=datetime.datetime.now, null=False)
+    deleted_at = models.DateTimeField(null=True)
+
 class persons(models.Model):
-    firs_name = models.CharField(max_length=50, null=False)
+    id_user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    id_exp_city  = models.ForeignKey(cities, on_delete=models.CASCADE)
+    firs_name = models.CharField(max_length=40, null=False)
     last_name = models.CharField(max_length=50, null=False)
     id_ident_type = models.IntegerField(null=False)
     ident_number = models.CharField(max_length=15, null=False)
@@ -36,15 +47,6 @@ class persons(models.Model):
     address = models.CharField(max_length=150, null=False)
     mobile = models.CharField(max_length=50, null=False)
     id_user = models.IntegerField(null=False)
-    created_at = models.DateTimeField(default=datetime.datetime.now, null=False)
-    updated_at = models.DateTimeField(default=datetime.datetime.now, null=False)
-    deleted_at = models.DateTimeField(null=True)
-
-class cities(models.Model):
-    name = models.CharField(max_length=100, null=False)
-    abrev = models.CharField(max_length=10, null=False)
-    descrip = models.CharField(max_length=10, null=False)
-    id_dept = models.IntegerField(null=False)
     created_at = models.DateTimeField(default=datetime.datetime.now, null=False)
     updated_at = models.DateTimeField(default=datetime.datetime.now, null=False)
     deleted_at = models.DateTimeField(null=True)
